6d0c4f594e2940f5f394750c5893ecaf175b68a6
Indra Wijaya <indra.wjya610@gmail.com>
2018-04-03 15:32:36 +0800

    Membuat Atribut dan Behavior

--- src/rumah/Home.java
+++ src/rumah/Home.java
@@ -10,5 +10,18 @@ package rumah;
  * @author Windows 10
  */
 public class Home {
+    String pintu;
+    String jendela;
+    String atap;
+    String warna;
+    double tipe;
     
+    
+    void cetakInfo(){
+        System.out.println("Jenis Pintu :"+pintu);
+        System.out.println("Kaca Jendela :"+jendela);
+        System.out.println("Warna Atap :"+atap); 
+        System.out.println("Warna Rumah :"+warna); 
+        System.out.println("Tipe Rumah :"+tipe); 
+    }
 }
